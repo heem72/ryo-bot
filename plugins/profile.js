9c626d6e35e6abfdfb0d4a6a824b6068679cf58b
heem72 <92344884+heem72@users.noreply.github.com>
2022-03-20 08:07:39 +0300

    Update profile.js

--- plugins/profile.js
+++ plugins/profile.js
@@ -8,22 +8,22 @@ let handler = async (m, { conn, usedPrefix }) => {
   } catch (e) {
 
   } finally {
-    let about = (await conn.getStatus(who).catch(console.error) || {}).status || ''
-    let { name, limit, exp, lastclaim, registered, regTime, age, level, role } = global.db.data.users[who]
-    let { min, xp, max } = levelling.xpRange(level, global.multiplier)
+    let الخبر📮 = (await conn.getStatus(who).catch(console.error) || {}).status || ''
+    let { الإسم🏷️, limit, exp, lastclaim, registered, regTime, age, level, role } = global.db.data.users[who]
+    let { min, اللفل, max } = levelling.xpRange(level, global.multiplier)
     let username = conn.getName(who)
     let math = max - xp
     let prem = global.prems.includes(who.split`@`[0])
     let str = `
-Name: ${username} ${registered ? '(' + name + ') ': ''}(@${who.replace(/@.+/, '')})${about ? '\nAbout: ' + about : ''}
-Number: ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
-Link: https://wa.me/${who.split`@`[0]}${registered ? '\nAge: ' + age : ''}
-XP: TOTAL ${exp} (${exp - min} / ${xp}) [${math <= 0 ? `Ready to *${usedPrefix}levelup*` : `${math} XP left to levelup`}]
-Level: ${level}
+الإسم💳: ${username} ${registered ? '(' + name + ') ': ''}(@${who.replace(/@.+/, '')})${about ? '\nAbout: ' + about : ''}
+الرقم#️⃣: ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
+رابط الرقم🔗: https://wa.me/${who.split`@`[0]}${registered ? '\nAge: ' + age : ''}
+الإكس بي🎚️: TOTAL ${exp} (${exp - min} / ${xp}) [${math <= 0 ? `Ready to *${usedPrefix}levelup*` : `${math} XP left to levelup`}]
+اللفل🎰: ${level}
 Role: *${role}*
-Limit: ${limit}
+حد ⚙️الإستخدام: ${limit}
 Registered: ${registered ? 'Yes (' + new Date(regTime) + ')': 'No'}
-Premium: ${prem ? 'Yes' : 'No'}${lastclaim > 0 ? '\nLast Claim: ' + new Date(lastclaim) : ''}
+: ${prem ? 'Yes' : 'No'}${lastclaim > 0 ? '\nLast Claim: ' + new Date(lastclaim) : ''}
 `.trim()
     let mentionedJid = [who]
     conn.sendFile(m.chat, pp, 'pp.jpg', str, m, false, { contextInfo: { mentionedJid }})
@@ -31,6 +31,6 @@ Premium: ${prem ? 'Yes' : 'No'}${lastclaim > 0 ? '\nLast Claim: ' + new Date(las
 }
 handler.help = ['profile [@user]']
 handler.tags = ['tools']
-handler.command = /^profile$/i
+handler.command = /^بروفايل$/i
 module.exports = handler
 
